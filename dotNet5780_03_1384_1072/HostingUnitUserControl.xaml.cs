007d0c9d01119fcce2172f94d0ac1b0e4b7dab5a
dovbaker <dovbaker@gmail.com>
2019-12-05 13:37:07 +0200

    vhvhh

--- dotNet5780_03_1384_1072/HostingUnitUserControl.xaml.cs
+++ dotNet5780_03_1384_1072/HostingUnitUserControl.xaml.cs
@@ -20,6 +20,11 @@ namespace dotNet5780_03_1384_1072
     /// </summary>
     public partial class HostingUnitUserControl : UserControl
     {
+
+        int imageIndex;
+        Viewbox vbImage;
+        Image MyImage;
+
         private Calendar MyCalendar;
         public HostingUnit CurrentHostingUnit
         { get; set; }
@@ -47,8 +52,27 @@ namespace dotNet5780_03_1384_1072
         //constructor
         public HostingUnitUserControl(HostingUnit hostUnit)
         {
+            vbImage = new Viewbox();
 
             InitializeComponent();
+
+            //size of pic
+            imageIndex = 0;
+            vbImage.Width = 75;
+            vbImage.Height = 75;
+            vbImage.Stretch = Stretch.Fill;
+            UserControlGrid.Children.Add(vbImage);
+            Grid.SetColumn(vbImage, 2);
+            Grid.SetRow(vbImage, 0);
+
+            //צרו תמונה חדשה, ושים אותה על ה ViewBox.
+            //MyImage = CreateViewImage();
+            //vbImage.Child = null;
+            //vbImage.Child = MyImage;
+
+
+
+
             this.CurrentHostingUnit = hostUnit;
             UserControlGrid.DataContext = hostUnit;
 
@@ -57,9 +81,44 @@ namespace dotNet5780_03_1384_1072
             vbCalendar.Child= MyCalendar;
             SetBlackOutDates();
 
-            
+            //vbImage.MouseUp += vbImage_MouseUp;
+            //vbImage.MouseEnter += vbImage_MouseEnter;
+            //vbImage.MouseLeave += vbImage_MouseLeave;
+
+
         }
 
+        //private Image CreateViewImage()
+        //{
+        //    Image dynamicImage = new Image();
+        //    BitmapImage bitmap = new BitmapImage();
+        //    bitmap.BeginInit();
+        //    bitmap.UriSource = new Uri(@CurrentHostingUnit.Uris[imageIndex]);
+        //    bitmap.EndInit();
+        //    // Set Image.Source
+        //    dynamicImage.Source = bitmap;
+        //    // Add Image to Window
+        //    return dynamicImage;
+        //}
+
+        //private void vbImage_MouseLeave(object sender, MouseEventArgs e)
+        //{
+        //    vbImage.Width = 75;
+        //    vbImage.Height = 75;
+        //}
+        //private void vbImage_MouseEnter(object sender, MouseEventArgs e)
+        //{
+        //    vbImage.Width = this.Width / 3;
+        //    vbImage.Height = this.Height;
+        //}
+        //private void vbImage_MouseUp(object sender, MouseButtonEventArgs e)
+        //{
+        //    vbImage.Child = null;
+        //    imageIndex =
+        //   (imageIndex + CurrentHostingUnit.Uris.Count - 1) % CurrentHostingUnit.Uris.Count;
+        //    MyImage = CreateViewImage();
+        //    vbImage.Child = MyImage;
+        //}
         private void btOrder_Click(object sender, RoutedEventArgs e)
         {
             List<DateTime> myList = MyCalendar.SelectedDates.ToList();
